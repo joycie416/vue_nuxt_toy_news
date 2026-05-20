d1ea65edfb8bc2b7aa658b2d2290e0fb87272726
joycie416 <joycie416@gmail.com>
2026-05-20 17:23:09 +0900

    feat: 헤더 추가

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
+  <div class="app">
+    <AppHeader />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+import { AppHeader } from "./components/common";
+</script>
+
+<style>
+body {
+  overflow-x: hidden; /** 수평 스크롤 숨기기 */
+}
+
+.app {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.page {
+  width: 100%;
+  max-width: 1132px;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  margin-top: 57px;
+  padding: 24px 0;
+
+  gap: 24px;
+}
+</style>
---
+++ app/components/common/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="flex items-center">
+      <img src="/assets/icons/icon-green.svg" alt="" class="h-10" />
+      <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">Nuxt4</h3>
+    </div>
+
+    <div class="flex items-center">
+      <Avatar>
+        <AvatarImage src="https://github.com//9diin.png" alt="profile-image" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  z-index: 1;
+
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 8px 24px;
+
+  background-color: #ffffff;
+  border-bottom: 1px solid #f5f5f5;
+}
+</style>
