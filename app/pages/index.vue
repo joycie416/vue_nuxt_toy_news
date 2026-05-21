a6926cb64b3cbcacd985fe1fc7c9dbc79cf66b69
joycie416 <joycie416@gmail.com>
2026-05-21 18:41:00 +0900

    refactor: useAsyncData로 리팩토링

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
     <AppBanner />
 
     <!-- 메인 뉴스 -->
-    <MainContent v-if="store.mainArticle" :data="store.mainArticle" />
-    <SkeletonMainContent v-else />
+    <SkeletonMainContent v-if="loading === 'pending'" />
+    <MainContent v-else :data="mainArticle" />
 
     <Separator label="Or" class="my-6" />
 
@@ -17,29 +17,51 @@
         <Button class="bg-red-500 hover:bg-red-600">See all</Button>
       </div>
       <div
-        v-if="!store.subArticles"
+        v-if="loading === 'pending'"
         class="w-full grid grid-cols-4 gap-y-20 gap-x-6"
       >
         <SkeletonSubContent />
         <SkeletonSubContent />
       </div>
       <div v-else class="w-full grid grid-cols-4 gap-y-20 gap-x-6">
-        <SubContent v-for="article in store.subArticles" :data="article" />
+        <SubContent v-for="article in subArticles" :data="article" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import axios from "axios";
 import { AppBanner, MainContent, SubContent } from "~/components/common";
 import { SkeletonMainContent, SkeletonSubContent } from "~/components/skeleton";
 import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
-import { useStore } from "~/store";
+import type { Article } from "~/types";
 
-const store = useStore();
+// useAsyncData를 사용하여 데이터를 비동기적으로 가져옵니다.
+const { data: newsData, status: loading } = useAsyncData("news", async () => {
+  const searchValue = "korea"; // 기본 검색어
+  const API_KEY = useRuntimeConfig().public.newsApiKey;
+  const API_URL = `https://newsapi.org/v2/everything?q=${searchValue}&from=2026-05-01&sortBy=popularity&apiKey=${API_KEY}`;
 
-onMounted(() => {
-  store.getNews();
+  try {
+    const response = await axios.get(API_URL);
+    const articles = response.data.articles
+      .map((article: Article) => {
+        if (article.urlToImage !== null) return article;
+      })
+      .filter((article: Article) => article !== undefined);
+
+    if (articles.length > 0) {
+      return { mainArticle: articles[0], subArticles: articles.slice(1) };
+    } else {
+      return { mainArticle: null, subArticles: [] };
+    }
+  } catch (error) {
+    throw error;
+  }
 });
+
+const mainArticle = newsData.value?.mainArticle ?? null; // null 또는 undefined 처리
+const subArticles = newsData.value?.subArticles ?? []; // 빈 배열로 기본값 처리
 </script>
